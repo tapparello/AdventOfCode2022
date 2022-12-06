24e31eb703eaeba787d79864eb4946fa069650bb
Cristiano Tapparello <cristiano.tapparello@rochester.edu>
2022-12-06 00:20:28 -0500

    Day 6

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -4,20 +4,20 @@
 using AdventOfCode;
 
 
-Console.WriteLine("Day 1:");
-new Day1().Execute();
+//Console.WriteLine("Day 1:");
+//new Day1().Execute();
 
-Console.WriteLine("Day 2:");
-new Day2().Execute();
+//Console.WriteLine("Day 2:");
+//new Day2().Execute();
 
-Console.WriteLine("Day 3:");
-new Day3().Execute();
+//Console.WriteLine("Day 3:");
+//new Day3().Execute();
 
-Console.WriteLine("Day 4:");
-new Day4().Execute();
+//Console.WriteLine("Day 4:");
+//new Day4().Execute();
 
-Console.WriteLine("Day 5:");
-new Day5().Execute();
+//Console.WriteLine("Day 5:");
+//new Day5().Execute();
 
 Console.WriteLine("Day 6:");
 new Day6().Execute();
